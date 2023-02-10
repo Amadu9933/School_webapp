c8a3ec69a8abfde527ffe8147e1c0a4692c90ec7
Amadu <amaduhamzajaarah300@gmail.com>
2023-02-10 13:10:49 +0000

    updated

--- models/base_model_2ndv.py
+++ models/base_model_2ndv.py
@@ -10,17 +10,16 @@ class BaseModel:
         if kwargs:
             for key, item in kwargs.items():
                 if key != "__class__":
-                    setattr(self, key, value)
+                    setattr(self, key, item)
                     if key in ["created_at", "updated_at"]:
-                        setattr(self, key, dt.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-
+                        setattr(self, key, dt.strptime(item, '%Y-%m-%dT%H:%M:%S.%f'))
         else:
             self.id = str(ui())
             self.created_at = dt.now()
             self.updated_at = dt.now()
 
     def __str__(self):
-        return f'[{type(self).__name__}, {self.id}, {self.__dict__}]'
+        return f"[{type(self).__name__}, {self.id}, {self.__dict__}]"
 
     def save(self):
         self.updated_at = dt.now()
